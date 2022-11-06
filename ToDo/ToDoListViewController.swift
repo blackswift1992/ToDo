9994bc147807980ac01f590fafe11c24f5271c70
Oleksii Moroz <blackswift1992@gmail.com>
2022-11-06 20:16:51 +0200

    implement UserDefault for toDoListItems saving when work is terminated

--- ToDo/ToDoListViewController.swift
+++ ToDo/ToDoListViewController.swift
@@ -1,21 +1,25 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-    var items = ["Buy milk", "Do my homework", "Pet my cat"]
+    private var toDoListItems = ["Buy milk", "Do my homework", "Pet my cat"]
+    private let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        if let defaultsToDoListItems = defaults.object(forKey: "toDoListItems") as? [String] {
+            toDoListItems = defaultsToDoListItems
+            tableView.reloadData()
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return toDoListItems.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.text = toDoListItems[indexPath.row]
         
         return cell
     }
@@ -67,17 +71,14 @@ private extension ToDoListViewController {
         
         let action = UIAlertAction(title: "Add task", style: .default) { [weak self] action in
             guard let text = textField?.text else { return }
-
-            self?.items.append(text)
             
-            DispatchQueue.main.async {
-                self?.tableView.reloadData()
-            }
+            self?.toDoListItems.append(text)
+            self?.defaults.set(self?.toDoListItems, forKey: "toDoListItems")
+            self?.tableView.reloadData()
+            
         }
         
         alert.addAction(action)
-        
-        
         
         present(alert, animated: true)
     }

a7536712d0223da8f1faca4e5f9ce44312dca336
Oleksii Moroz <blackswift1992@gmail.com>
2022-11-16 02:46:55 +0200

    set relationship between ToDoItem and ToDoCategory

--- ToDo/Model/ToDoCategory.swift
+++ ToDo/Model/ToDoCategory.swift
@@ -3,4 +3,5 @@ import RealmSwift
 
 class ToDoCategory: Object {
     @objc dynamic var name: String = ""
+    let items = List<ToDoItem>()
 }
--- ToDo/Model/ToDoItem.swift
+++ ToDo/Model/ToDoItem.swift
@@ -4,4 +4,5 @@ import RealmSwift
 class ToDoItem: Object {
     @objc dynamic var taskName: String = ""
     @objc dynamic var isDone: Bool = false
+    var parentCategory = LinkingObjects(fromType: ToDoCategory.self, property: "items")
 }
